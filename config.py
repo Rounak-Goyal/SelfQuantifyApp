d9c36b20e221782b03ed72695ecdb8054cec71cc
rounak <rounak31@gmail.com>
2022-09-14 07:14:35 +0530

    hosted

--- config.py
+++
@@ -1,4 +0,0 @@
-class DefCon():
-    #SQLALCHEMY_DATABASE_URI = "sqlite:///finaldatabase.sqlite3"
-    SQLALCHEMY_DATABASE_URI = "postgresql://vplncarjbjcoti:114dbbb8cb6216bb04d76c526dc108fadd31b9621be7cb64a7e0cd35762795ef@ec2-34-234-240-121.compute-1.amazonaws.com:5432/da4tpjiifm112i"
-    
